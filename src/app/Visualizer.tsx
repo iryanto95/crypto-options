88b5f196f1c6f3511efafab1b239bfd2d6a6be65
iryanto95 <iryanto62@gmail.com>
2025-03-12 22:28:58 +0800

    fixed calendar date, added cols count

--- src/app/Visualizer.tsx
+++ src/app/Visualizer.tsx
@@ -14,7 +14,7 @@ import NeuTypography from './components/NeuTypography'
 import { blackScholes, calculateIV } from './utils/calc'
 
 const STRIKE_INCR: { [key: string]: number } = {
-  'BTCUSDT': 2000,
+  'BTCUSDT': 1000,
   'ETHUSDT': 25,
   'SOLUSDT': 2,
   'BNBUSDT': 5,
@@ -32,6 +32,8 @@ type propsType = {
   displayDay: string,
 }
 
+const cols = 10
+
 export default function Visualizer(props: propsType) {
   const [lowestDTE, setLowestDTE] = useState(0)
 
@@ -48,46 +50,50 @@ export default function Visualizer(props: propsType) {
 
   const rows = []
   const dates = []
-  for (let day = -1; day < 10; day++) {
-    const theDate = new Date(today.getTime() + (day + 1 + lowestDTE) * 24 * 3600 * 1000)
+  for (let day = -1; day < cols; day++) {
     if (day === -1)
       dates.push(
-        <Grid key={day} size={12/11} sx={{ textAlign: 'right', fontSize: '10pt', padding: '2px' }}>
+        <Grid key={day} size={12/(cols + 1)} sx={{ textAlign: 'right', fontSize: '10pt', padding: '2px' }}>
           <NeuTypography fontSize={12}>Strike\{ props.displayDay === 'expd' ? 'Expiry' : 'DTE' }</NeuTypography>
         </Grid>
       )
-    else
+    else {
+      const theDate = new Date(today.getTime() + (day + lowestDTE) * 24 * 3600 * 1000)
       dates.push(
-        <Grid key={day} size={12/11} sx={{ textAlign: 'center', padding: '2px', borderBottom: '0.01px solid rgba(51, 27, 95, 0.30)', }}>
+        <Grid key={day} size={12/(cols + 1)} sx={{ textAlign: 'center', padding: '2px', borderBottom: '0.01px solid rgba(51, 27, 95, 0.30)', }}>
           { props.displayDay === 'expd' ?
             <NeuTypography fontSize={12}>{`${theDate.getDate()}/${theDate.getMonth() + 1}/${theDate.getFullYear()}`}</NeuTypography>
-            : <NeuTypography fontSize={12}>{day + lowestDTE + 1}</NeuTypography>
+            : <NeuTypography fontSize={12}>{day + lowestDTE}</NeuTypography>
           }
         </Grid>
       )
+    }
   }
   rows.push(dates)
 
   let maxDiff = 0
   const cells: {[key: number]: Array<{diff: number, iv: string, optPrice: number, bsPrice: number, isRef: boolean}>} = {}
 
-  for (let strike = refPrice + 5 * incr; strike >= refPrice - 5 * incr; strike -= incr) {
+  for (let strike = refPrice + 12 * incr; strike >= refPrice - 12 * incr; strike -= incr) {
     const row = []
     row.push(
-      <Grid key={strike} size={12/11} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'right', paddingRight: '8px', fontSize: '10pt', borderRight: '0.01px solid rgba(51, 27, 95, 0.30)' }}>
+      <Grid key={strike} size={12/(cols + 1)} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'right', paddingRight: '8px', fontSize: '10pt', borderRight: '0.01px solid rgba(51, 27, 95, 0.30)' }}>
         <NeuTypography fontSize={12}>{strike.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</NeuTypography>
       </Grid>
     )
 
     const strikeCells = []
-    for (let day = 1; day < 11; day++) {
-      const thisDay = new Date(today.getTime() + day * 24 * 2600 * 1000)
+    for (let day = 0; day < cols; day++) {
+      const thisDay = new Date(today.getTime() + ( lowestDTE + day ) * 24 * 3600 * 1000)
       const thisDayString = `${thisDay.getFullYear() % 100}${thisDay.getMonth() + 1 < 10 ? 0 : ''}${thisDay.getMonth() + 1}${thisDay.getDate() < 10 ? 0 : ''}${thisDay.getDate()}`
       
       const optPrice = props.marketPrices[`${props.pair.substring(0, props.pair.length - 4)}-${thisDayString}-${(strike).toFixed(2)}-${props.optionType === 'call' ? 'C' : 'P'}`]
       const bsPrice = blackScholes(props.currentPrice, strike, (day + lowestDTE + remainingToday) / 365, props.riskFreeRate, props.hv, props.optionType)
       const iv = optPrice ? calculateIV(optPrice, props.currentPrice, strike, (day + lowestDTE + remainingToday) / 365, props.riskFreeRate, props.hv, props.optionType) : null
       const diff = optPrice > 0 ? optPrice - bsPrice : 0
+      
+      if (strike === refPrice && props.optionType === 'call') 
+        console.log(day, `${props.pair.substring(0, props.pair.length - 4)}-${thisDayString}-${(strike).toFixed(2)}-${props.optionType === 'call' ? 'C' : 'P'}`, optPrice)
 
       if (Math.abs(diff) > maxDiff)
         maxDiff = Math.abs(diff)
@@ -102,16 +108,16 @@ export default function Visualizer(props: propsType) {
     }
     cells[strike] = strikeCells
   }
-  for (let strike = refPrice + 5 * incr; strike >= refPrice - 5 * incr; strike -= incr) {
+  for (let strike = refPrice + 12 * incr; strike >= refPrice - 12 * incr; strike -= incr) {
     const row = []
     row.push(
-      <Grid key={strike} size={12/11} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'right', paddingRight: '8px', fontSize: '10pt', borderRight: '0.01px solid rgba(51, 27, 95, 0.30)' }}>
+      <Grid key={strike} size={12/(cols + 1)} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'right', paddingRight: '8px', fontSize: '10pt', borderRight: '0.01px solid rgba(51, 27, 95, 0.30)' }}>
         <NeuTypography fontSize={12}>{strike.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</NeuTypography>
       </Grid>
     )
       for (const cell of cells[strike]) {
         row.push(
-          <Grid key={row.length} size={12/11} 
+          <Grid key={row.length} size={12/(cols + 1)} 
             sx={{
               display: 'flex',
               alignItems: 'center',
@@ -127,7 +133,7 @@ export default function Visualizer(props: propsType) {
                 : cell.diff > 0 ? 
                 `rgba(200,100,130,${0.5 + 0.5 * Math.abs(cell.diff / maxDiff)})` : undefined
             }}>
-              <Box >
+              <Box>
                 <Box sx={{visibility: cell.optPrice ? 'visible' : 'hidden', fontWeight: 600}}><NeuTypography fontSize={12}>{cell.optPrice ? cell.optPrice.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 }) : <>-</>}</NeuTypography></Box>
                 <Box>
                   { props.displayVal === 'fv' ? 
@@ -148,10 +154,10 @@ export default function Visualizer(props: propsType) {
         <NeuTypography variant='h5'>{props.optionType === 'call' ? 'Calls' : 'Puts'}</NeuTypography>
       </Box>
       <Box sx={{position: 'absolute', top: '0px', right: '0px'}}>
-        <NeuIconButton sx={{marginRight: '16px'}} disabled={lowestDTE === 0} onClick={() => setLowestDTE(lowestDTE - 10)}>
+        <NeuIconButton sx={{marginRight: '16px'}} disabled={lowestDTE === 0} onClick={() => setLowestDTE(lowestDTE - cols)}>
           <ArrowLeft sx={{color: 'rgba(51, 27, 95, 0.8)'}}/>
         </NeuIconButton>
-        <NeuIconButton disabled={lowestDTE === 90} onClick={() => setLowestDTE(lowestDTE + 10)}>
+        <NeuIconButton disabled={lowestDTE === 90} onClick={() => setLowestDTE(lowestDTE + cols)}>
           <ArrowRight sx={{color: 'rgba(51, 27, 95, 0.8)'}}/>
         </NeuIconButton>
       </Box>
